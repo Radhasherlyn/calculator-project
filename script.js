95f2a26fff27f74c89a377088e76279480d5609a
Radhasherlyn <radhasherlyngujjula@gmail.com>
2026-04-14 00:36:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function scrollToSection() {
-    document.getElementById("projects").scrollIntoView({
-        behavior: "smooth"
-    });
+let expression = "";
+
+let expressionDisplay = document.getElementById("expression");
+let resultDisplay = document.getElementById("result");
+
+function appendValue(value){
+expression += value;
+expressionDisplay.textContent = expression;
+}
+
+function clearDisplay(){
+expression="";
+expressionDisplay.textContent="";
+resultDisplay.textContent="";
+}
+
+function calculate(){
+try{
+let result = eval(expression);
+resultDisplay.textContent = result;
+}catch{
+resultDisplay.textContent="Error";
+}
 }
